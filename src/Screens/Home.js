b5dbffddc07424fb8967cd5ff31da5221460b568
Katanis <emiliskakanis@gmail.com>
2020-06-02 15:57:44 +0300

    init and file structure

---
+++ src/Screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1 style={style.header}>Home Page</h1>
+    </div>
+  );
+};
+
+const style = {
+  header: {
+    color: 'red',
+    fontSize: '18px',
+  },
+};
+
+export default Home;
