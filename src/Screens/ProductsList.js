b6012f10ce69d566733d4779ce0f8c871f253554
Katanis <emiliskakanis@gmail.com>
2020-06-08 13:59:07 +0300

    Product list view shows items from localStorage

--- src/Screens/ProductsList.js
+++ src/Screens/ProductsList.js
@@ -1,59 +1,52 @@
-import React from 'react';
-
-const data = {
-  prod1: {
-    name: 'table',
-    EAN: 125,
-    type: 'furniture',
-    weight: 20,
-    color: 'brown',
-    active: true,
-  },
-  prod2: {
-    name: 'lamp',
-    EAN: 125,
-    type: 'furniture',
-    weight: 20,
-    color: 'brown',
-    active: true,
-  },
-  prod3: {
-    name: 'TW',
-    EAN: 125,
-    type: 'furniture',
-    weight: 20,
-    color: 'brown',
-    active: true,
-  },
-};
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductsPreview = (props) => {
-  return <ProductList></ProductList>;
+  const [deleted, setDelete] = useState(false);
+
+  function deleteHandler(key) {
+    localStorage.removeItem(key);
+    setDelete(!deleted);
+    alert("Product with id: " + key + " deleted!");
+  }
+  return <ProductList deleteHandler={deleteHandler}></ProductList>;
 };
 
 const ProductList = (props) => {
   return (
     <div style={style.container}>
       <TableHeader></TableHeader>
-      <div>
-        {Object.entries(data).map(([key, value]) => {
-          return <p>{value.name}</p>;
-        })}
-      </div>
+      {Object.entries(localStorage).map(([key, valueJSON]) => {
+        console.log(localStorage.getItem('name'));
+        const value = JSON.parse(valueJSON);
+        return (
+          <div style={style.gridItemsContainer}>
+            <p>{value.name}</p>
+            <p>{value.ean}</p>
+            <p>{value.type}</p>
+            <p>{value.weight}</p>
+            <p>{value.color}</p>
+            <input checked={value.active} type="checkbox"></input>
+            <Link to={'/products/' + key}>VIEW</Link>
+            <Link to={'/products/' + key + '/edit'}>EDIT</Link>
+            <button onClick={() => props.deleteHandler(key)}>DELETE</button>
+          </div>
+        );
+      })}
+      <Link to="/products/create">Add new product</Link>
     </div>
   );
 };
 
 const TableHeader = (props) => {
   return (
-    <div style={style.tavbleHeaderContainer}>
+    <div style={style.gridTableHeader}>
       <TableField style={style.field} text="Name"></TableField>
       <TableField style={style.field} text="EAN"></TableField>
       <TableField style={style.field} text="Type"></TableField>
       <TableField style={style.field} text="Weight"></TableField>
       <TableField style={style.field} text="Color"></TableField>
       <TableField style={style.field} text="Active"></TableField>
-      <TableField style={style.field} text="ACTION"></TableField>
     </div>
   );
 };
@@ -73,6 +66,15 @@ const style = {
     color: 'red',
     fontSize: '18px',
     fontWeight: '500',
+  },
+  gridTableHeader: {
+    display: 'grid',
+    gridTemplateColumns: '10% 10% 10% 10% 10% 10%',
+    // gridTemplateRows: 'auto auto auto',
+  },
+  gridItemsContainer: {
+    display: 'grid',
+    gridTemplateColumns: '10% 10% 10% 10% 10% 10% 10% 10% 10%',
   },
   field: {
     margin: '10px',
